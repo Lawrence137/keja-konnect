cd8107882bcc28465104b90587d6c4e1d19442e1
Lawrence Kimani <lawrencekimani18@gmail.com>
2025-04-09 20:39:36 +0300

    changed hamburger menu css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 hover:bg-gray-100 transition-colors"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 hover:bg-gray-100 transition-colors border border-green-800 shadow-sm"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
